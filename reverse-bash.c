94fb4dee195c54503f61f3587876985f8ce58fcd
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-07-07 18:00:44 -0600

    Add files via upload

---
+++ reverse-bash.c
@@ -0,0 +1,16 @@
+/*
+  Name:
+  Date:
+
+  Description: Echoes the given word in terminal.
+               Using the parameters in main & how it can
+               be used to run the progrma via terminal.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv) {
+  
+  
+  return 0;
+}
