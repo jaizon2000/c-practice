37aedf33fa18526a0ad878cd97470662fe5e9867
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-07-07 19:29:08 -0600

    finished

--- reverse-bash.c
+++ reverse-bash.c
@@ -1,6 +1,6 @@
 /*
-  Name:
-  Date:
+  Name: reverse-bash.c
+  Date: July 7, 2020
 
   Description: Echoes the given word in terminal.
                Using the parameters in main & how it can
@@ -11,13 +11,13 @@
 #include <string.h> // strlen
 
 int main(int argc, char *argv[]) {
-  int NUMBER_OF_WORDS = argc - 1;
+  int NUMBER_OF_WORDS = argc - 1; // take out null
 
-  char reversed_array[NUMBER_OF_WORDS];
-
-  for (int i = 0, j = NUMBER_OF_WORDS; i != 0; i++, j--) {
-    // from last item of argv, put last as first in reversed array
-    reversed_array[i] = *argv[j];
+  // Go through argv backwards
+  for (int i = NUMBER_OF_WORDS; i > 0; i--) {
+    printf("%s ", argv[i]);
   }
+  printf("\n");
+  
   return 0;
 }
