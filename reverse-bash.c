5ea863eb0f32b0d316036060088b1fe04608efe0
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-07-08 16:24:52 -0600

    Update and refined

--- reverse-bash.c
+++ reverse-bash.c
@@ -4,17 +4,17 @@
 
   Description: Echoes the given word in terminal.
                Using the parameters in main & how it can
-               be used to run the progrma via terminal.
+               be used to run the program via terminal.
 */
 
 #include <stdio.h>
 #include <string.h> // strlen
 
 int main(int argc, char *argv[]) {
-  int NUMBER_OF_WORDS = argc - 1; // take out null
+  int number_of_words = argc - 1;
 
   // Go through argv backwards
-  for (int i = NUMBER_OF_WORDS; i > 0; i--) {
+  for (int i = number_of_words; i > 0; i--) {
     printf("%s ", argv[i]);
   }
   printf("\n");
