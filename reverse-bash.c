a185cfeb6a4073a86645893a08c62d2b9cc35d42
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-07-07 19:00:54 -0600

    Update

--- reverse-bash.c
+++ reverse-bash.c
@@ -8,9 +8,16 @@
 */
 
 #include <stdio.h>
+#include <string.h> // strlen
 
-int main(int argc, char *argv) {
-  
-  
+int main(int argc, char *argv[]) {
+  int NUMBER_OF_WORDS = argc - 1;
+
+  char reversed_array[NUMBER_OF_WORDS];
+
+  for (int i = 0, j = NUMBER_OF_WORDS; i != 0; i++, j--) {
+    // from last item of argv, put last as first in reversed array
+    reversed_array[i] = *argv[j];
+  }
   return 0;
 }
