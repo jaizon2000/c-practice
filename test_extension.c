82935d57e97aaefe2908426382490455b041096b
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2020-07-26 17:14:58 -0600

    Add files via upload

--- test_extension.c
+++ test_extension.c
@@ -1,8 +1,8 @@
 /*
-  Name:
-  Date:
+  Name: test_extension.c
+  Date: July 26, 2020
 
-  Description:
+  Description: Check if file extension matches the given extension
 */
 
 #include <stdio.h>
@@ -10,14 +10,28 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+#define MAX_EXTENSION_FILE_LENGTH 5
+
 bool test_extension(const char *fname, const char *extension) {
+  int i = 0;
   while (*fname != '.') {
-    putchar(*fname);
     fname++;
+    i++;
   }
+  fname++; // pointer will point to char after '.'  
+ 
+  while (*fname) {
+    if (*extension != toupper(*fname)) {
+      return false;
+    }
+    fname++;
+    extension++;
+  } 
+  return true;
 }
 
 int main() {
-  testexttension("memo.txt", "TXT");
+  puts("testing: test_extension(\"memo.txt\", \"TXT\")");
+  printf("result: %s\n", test_extension("memo.txt", "TXT") ? "true" : "false");
   return 0;
 }
